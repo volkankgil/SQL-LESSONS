db53bedc35013194d29f3d6d68cc2a1db32c155e
volkankgil <104678474+volkankgil@users.noreply.github.com>
2022-09-18 20:48:58 +0300

    ödev1

--- homework1.sql
+++ homework1.sql
@@ -8,3 +8,12 @@ WHERE first_name='Mary'
 
 SELECT * FROM film
 WHERE length<50 AND rental_rate !=2.99 AND rental_rate!=4.99
+
+
+  Aþaðýdaki sorgu senaryolarýný dvdrental örnek veri tabaný üzerinden gerçekleþtiriniz.
+
+  film tablosunda bulunan title ve description sütunlarýndaki verileri sýralayýnýz.
+  film tablosunda bulunan tüm sütunlardaki verileri film uzunluðu (length) 60 dan büyük VE 75 ten küçük olma koþullarýyla sýralayýnýz.
+  film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koþullarýyla sýralayýnýz.
+  customer tablosunda bulunan first_name sütunundaki deðeri 'Mary' olan müþterinin last_name sütunundaki deðeri nedir?
+  film tablosundaki uzunluðu(length) 50 ten büyük OLMAYIP ayný zamanda rental_rate deðeri 2.99 veya 4.99 OLMAYAN verileri sýralayýnýz.
